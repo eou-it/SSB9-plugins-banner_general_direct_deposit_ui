9b786b94118e6affb7e35e2fdfded108c5fcbbbf
Jim Caley <jim.caley@ellucian.com>
2017-03-17 15:47:58 -0400

    Update copyright.

--- web-app/directDepositApp/app.js
+++ web-app/directDepositApp/app.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- Copyright 2015 Ellucian Company L.P. and its affiliates.
+ Copyright 2017 Ellucian Company L.P. and its affiliates.
  *******************************************************************************/
 var generalSsbAppControllers = angular.module('generalSsbAppControllers', []);
 var generalSsbAppDirectives = angular.module('generalSsbAppDirectives', []);
